746e1680331270a89624aa10e288e066d9bbe0f9
Thomas Riley <thomas@Thomass-MacBook-Pro.local>
2023-05-01 12:30:36 +0100

    added fetch to local server

--- client/src/components/OurPicks.js
+++ client/src/components/OurPicks.js
@@ -1,10 +1,15 @@
 import Podcast from "./Podcast";
 
-const OurPicks = () => {
+const OurPicks = ({shows}) => {
+
+    const ourShows = shows.map((show, index) => {
+        return <Podcast show ={show} key={index} />
+    })
+
     return ( 
         <>
             <h4>Our Picks</h4>
-            <Podcast />
+            {ourShows}
         </>
      );
 }
