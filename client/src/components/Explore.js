8eadc6bdb4e434b3f7f191c66277c8dbc931e4fe
Lewis Bonnar <lewisbonnar74@gmail.com>
2023-05-02 11:58:29 +0100

    added explore page with sort buttons

---
+++ client/src/components/Explore.js
@@ -0,0 +1,51 @@
+import Podcast from "./Podcast";
+import React, { useState, useEffect } from "react";
+
+const Explore = ({ shows }) => {
+  const [sortedShows, setSortedShows] = useState(shows);
+  const [searchText, setSearchText] = useState("");
+
+  const sortedShowsToMapThrough = sortedShows.map((show, index) => {
+    return <Podcast show={show} key={index} />;
+  });
+
+  const searchShows = (event) => {
+    setSearchText(event.target.value);
+    const searchedShows =
+      searchText === ""
+        ? shows
+        : shows.filter(
+            (show) =>
+              show.description &&
+              show.description.toLowerCase().includes(searchText.toLowerCase())
+          );
+    setSortedShows(searchedShows);
+  };
+
+  const sortKidFriendly = () => {
+    const kidFriendlyShows = shows.filter((show) => show.explicit === false);
+    setSortedShows(kidFriendlyShows);
+  };
+
+  const sortAToZ = () => {
+    const aToZShows = shows.sort((a, b) => (a.name > b.name ? 1 : -1));
+    setSortedShows(aToZShows);
+  };
+
+  return (
+    <>
+      <input
+        type="text"
+        value={searchText}
+        onChange={searchShows}
+        placeholder="Search keyword..."
+      />
+      <button onClick={sortKidFriendly}>Kid Friendly Options!</button>
+      <button onClick={sortAToZ}>All Shows A-Z</button>
+
+      {sortedShowsToMapThrough}
+    </>
+  );
+};
+
+export default Explore;
